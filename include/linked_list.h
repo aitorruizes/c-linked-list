2e75cbbd50e4f2cfff2af9c59eb4f797e22e814b
Aitor Ruiz <aitorruizagra.es@gmail.com>
2025-01-28 11:55:27 -0300

    feat: add generic linked list with user-defined data handling functions
    - Introduced `NodeData` typedef as a generic pointer type for node data.
    - Created `Node` and `LinkedList` structs with support for storing and managing arbitrary data types.
    - Added function pointers for user-defined operations: `print_data_function`, `free_data_function`, and `compare_data_function` for custom handling of node data.

--- include/linked_list.h
+++ include/linked_list.h
@@ -1,3 +1,21 @@
 #ifndef LINKED_LIST_H
 #define LINKED_LIST_H
+
+typedef void *NodeData;
+
+typedef struct Node
+{
+  NodeData node_data;
+  struct Node *next_node;
+} Node;
+
+typedef struct LinkedList
+{
+  Node *head_node;
+  Node *tail_node;
+  void (*print_data_function)(NodeData *);
+  void (*free_data_function)(NodeData *);
+  void (*compare_data_function)(NodeData *, NodeData *);
+} LinkedList;
+
 #endif
